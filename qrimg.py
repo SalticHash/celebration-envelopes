acfb4f0a0b865a31d1f5c157aacc9d544d63e132
SalticHash <104740982+SalticHash@users.noreply.github.com>
2024-04-22 18:32:11 -0600

    Finishing touches

--- qrimg.py
+++ qrimg.py
@@ -2,18 +2,15 @@
 from io import BytesIO
 
 # others
-from qrcode import QRCode
+from qrcode import make as makeQR
 from flask import Response
 
 def generate_qr_image(data: str) -> Response:
-    qr = QRCode()
-    qr.add_data(data)
-
-    img = qr.make_image()
+    qr_img = makeQR(data)
 
     # Create a BytesIO object to hold the image data in memory
     img_io = BytesIO()
-    img.save(img_io, format='PNG')
+    qr_img.save(img_io, format='PNG')
     img_io.seek(0)
 
     # Return the image as a response with appropriate headers
